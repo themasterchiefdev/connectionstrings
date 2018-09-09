1d4ce2f62f3025ba1b00d153deed5eae2a31cc5d
codeandcofee <yvrkarthik@gmail.com>
2018-09-10 08:03:26 +1200

    added input group to select the database provider

--- src/components/connectionstrings.tsx
+++ src/components/connectionstrings.tsx
@@ -21,31 +21,46 @@ export class ConnectionStrings extends Component<{}, IMyComponentState> {
 
   public render(): JSX.Element {
     return (
-      <div>
-        <table className="table">
-          <thead>
-            <tr className="text-left">
-              <th scope="col">Database Name</th>
-              <th scope="col">Connection String Details</th>
-            </tr>
-          </thead>
-          <tbody>
+      <React.Fragment>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">
+              Select the Database Provider
+            </label>
+          </div>
+          <select className="custom-select" id="inputGroupSelect01">
+            <option selected={true}>Choose...</option>
             {this.state.connStrings.map(cs => (
-              <tr key={cs.databaseName} className="text-left">
-                <td>{cs.databaseName}</td>
-                <td>
-                  {cs.connectionStringDetails.map((sd, index) => (
-                    <React.Fragment key={index}>
-                      <div className="font-weight-bold">{sd.description}</div>
-                      <div>{sd.connectionString}</div>
-                    </React.Fragment>
-                  ))}
-                </td>
-              </tr>
+              <option value={cs.databaseName}>{cs.databaseName}</option>
             ))}
-          </tbody>
-        </table>
-      </div>
+          </select>
+        </div>
+        <div>
+          <table className="table">
+            <thead>
+              <tr className="text-left">
+                <th scope="col">Database Name</th>
+                <th scope="col">Connection String Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.connStrings.map(cs => (
+                <tr key={cs.databaseName} className="text-left">
+                  <td>{cs.databaseName}</td>
+                  <td>
+                    {cs.connectionStringDetails.map((sd, index) => (
+                      <React.Fragment key={index}>
+                        <div className="font-weight-bold">{sd.description}</div>
+                        <div>{sd.connectionString}</div>
+                      </React.Fragment>
+                    ))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
     );
   }
 
